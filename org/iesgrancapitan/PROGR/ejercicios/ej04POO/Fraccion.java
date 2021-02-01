45180065916ea8db75616c28729ade78dcf7bd2a
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-02-01 21:30:22 +0100

    Clase Fracción

--- org/iesgrancapitan/PROGR/ejercicios/ej04POO/Fraccion.java
+++ org/iesgrancapitan/PROGR/ejercicios/ej04POO/Fraccion.java
@@ -1,326 +1,167 @@
 package org.iesgrancapitan.PROGR.ejercicios.ej04POO;
 
 /**
- * Implementación de la clase Fracción.
+ * clase Fraccion de forma que podamos hacer las siguientes operaciones:
  * 
- * @author Rafael del Castillo
+ * Contruir un objeto Fraccion pasándole el numerador y el denominador.
+ * Obtener la fracción como cadena de caracteres.
+ * Obtener y modificar numerador y denominador. No se puede dividir por cero.
+ * Obtener resultado de la fracción (número real).
+ * Multiplicar la fracción por un número (el resultado es otro objeto fracción).
+ * Multiplicar, sumar y restar fracciones (el resultado es otro objeto fracción).
+ * Simplificar la fracción (cambia el objeto actual).
+
+ * @author Rafael del Castillo Gomariz
+ *
  */
 
-public class Fraccion implements Comparable<Fraccion> {
-  private int num;      // numerador
-  private int den;      // denominador
+public class Fraccion implements Comparable<Fraccion>, Cloneable {
+  
+  // atributos
+  
+  private int numerador;
+  private int denominador;
+  
+  //comportamiento
+  
+  /**
+   * Constructor.
+   * 
+   * No vamos a permitir como denominador cero.
+   * 
+   * @param numerador
+   * @param denominador
+   */
+  public Fraccion(int numerador, int denominador) {
+    this.numerador = numerador;
+    this.setDenominador(denominador);
+  }
+  
+  // getters, setters y toString()
 
   /**
-   * Constructor de la Fracción.
-   * @param x numerador de la fracción.
-   * @param y denominador de la fracción, debe ser mayor que cero.
+   * @return the numerador
    */
-  public Fraccion(int x, int y) {
-    setNum(x);
-    setDen(y);
+  public int getNumerador() {
+    return numerador;
   }
 
   /**
-   * @return numerador de la fracción.
+   * @param numerador the numerador to set
    */
-  public int getNum() {
-    return num;
+  public void setNumerador(int numerador) {
+    this.numerador = numerador;
   }
 
   /**
-   * Asigna numerador a la fracción.
-   * @param num numerador de la fracción.
+   * @return the denominador
    */
-  public void setNum(int num) {
-    this.num = num;
+  public int getDenominador() {
+    return denominador;
   }
 
   /**
-   * @return denominador de la fracción.
+   * @param denominador the denominador to set
    */
-  public int getDen() {
-    return den;
-  }
-
-  /**
-   * Asigna denominador a la fracción.
-   * @param den denominador de la fracción. Debe ser mayor o igual que cero.
-   * @throws ArithmeticException
-   */
-  public void setDen(int den) throws ArithmeticException {
-    if (den == 0) {
-      throw new ArithmeticException();
+  public void setDenominador(int denominador) {
+    this.denominador = denominador;
+    
+    // si el denominador es 0 deberíamos lanzar una excepción
+    // pero como aún no las hemos visto, vamos a mostrar un mensaje de error
+    if (denominador == 0) {
+      System.err.println("El estado del objeto es incorrecto al ser el denominador 0.");
     }
-    this.den = den;
-  }
-
-  /**
-   * Suma fracciones.
-   * @param a primer sumando.
-   * @param b segundo sumando.
-   * @return suma de las fracciones.
-   */
-  public static Fraccion sumar(Fraccion a, Fraccion b) {
-    Fraccion f = new Fraccion(a.num * b.den + b.num * a.den, a.den * b.den);
-    f.simplifica();
-    return f;
-  }
-  
-  /**
-   * Suma una fracción con un entero.
-   * @param a primer sumando.
-   * @param b segundo sumando.
-   * @return suma.
-   */
-  public static Fraccion sumar(Fraccion a, int b) {
-    return sumar(a, new Fraccion(b, 1));
-  }
-  
-  /**
-   * Suma un entero con una fracción.
-   * @param a primer sumando.
-   * @param b segundo sumando.
-   * @return suma.
-   */
-  public static Fraccion sumar(int a, Fraccion b) {
-    return sumar(b, a);
-  }
-  
-  /**
-   * Incrementa el objeto Fracción actual.
-   * @param a fracción a incrementar.
-   */
-  public void incrementa(Fraccion a) {
-    Fraccion f = sumar(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-  
-  /**
-   * Incrementa el objeto Fracción actual.
-   * @param a entero a incrementar.
-   */
-  public void incrementa(int a) {
-    incrementa(new Fraccion(a, 1));
-  }
-  
-  /**
-   * Resta fracciones.
-   * @param a minuendo.
-   * @param b sustraendo.
-   * @return resta de las fracciones
-   */
-  public static Fraccion restar(Fraccion a, Fraccion b) {
-    Fraccion f = new Fraccion(a.num * b.den - b.num * a.den, a.den * b.den);
-    f.simplifica();
-    return f;
-  }
-  
-  /**
-   * Resta a una fracción un entero.
-   * @param a minuendo.
-   * @param b sustraendo.
-   * @return resta.
-   */
-  public static Fraccion restar(Fraccion a, int b) {
-    return restar(a, new Fraccion(b, 1));
-  }
-  
-  /**
-   * Resta a un entero una fracción.
-   * @param a minuendo.
-   * @param b sustraendo.
-   * @return resta.
-   */
-  public static Fraccion restar(int a, Fraccion b) {
-    return restar(new Fraccion(a, 1), b);
-  }
-  
-  /**
-   * Decrementa el objeto Fracción actual.
-   * @param a fracción a decrementar.
-   */
-  
-  public void decrementa(Fraccion a) {
-    Fraccion f = restar(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-  
-  /**
-   * Decrementa el objeto Fracción actual.
-   * @param a entero a decrementar.
-   */
-  
-  public void decrementa(int a) {
-    Fraccion f = restar(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-  
-  /**
-   * Multiplica fracciones.
-   * @param a factor multiplicando.
-   * @param b factor multiplicador.
-   * @return producto de las fracciones.
-   */
-  public static Fraccion multiplicar(Fraccion a, Fraccion b) {
-    Fraccion f = new Fraccion(a.num * b.num, a.den * b.den);
-    f.simplifica();
-    return f;
-  }
-  
-  /**
-   * Multiplicación escalar.
-   * @param a factor multiplicando.
-   * @param b factor multiplicador.
-   * @return producto.
-   */
-  public static Fraccion multiplicar(Fraccion a, int b) {
-    return multiplicar(a, new Fraccion(b, 1));
-  }
-  
-  /**
-   * Multiplicación escalar.
-   * @param a factor multiplicando.
-   * @param b factor multiplicador.
-   * @return producto.
-   */
-  public static Fraccion multiplicar(int a, Fraccion b) {
-    return multiplicar(b, a);
-  }
-  
-  /**
-   * Multiplica la fracción actual por la recibida como parámetro y actualiza numerador y denominador.
-   * @param a fracción a multiplicar.
-   */
-  public void multiplica(Fraccion a) {
-    Fraccion f = multiplicar(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-  
-  /**
-   * Multiplica la fracción actual por el entero recibido como parámetro y actualiza numerador y denominador.
-   * @param a entero a multiplicar.
-   */
-  public void multiplica(int a) {
-    Fraccion f = multiplicar(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-
-  /**
-   * Divide fracciones.
-   * @param a dividendo.
-   * @param b divisor.
-   * @return cociente de las fracciones.
-   */
-  public static Fraccion dividir(Fraccion a, Fraccion b) {
-    Fraccion f = new Fraccion(a.num * b.den, a.den * b.num);
-    f.simplifica();
-    return f;
-  }
-  
-  /**
-   * División escalar.
-   * @param a dividendo.
-   * @param b divisor.
-   * @return cociente.
-   */
-  public static Fraccion dividir(Fraccion a, int b) throws ArithmeticException {
-    if (b == 0) {
-      throw new ArithmeticException();
-    }  
-    return dividir(a, new Fraccion(b, 1));
-  }
-  
-  /**
-   * División escalar.
-   * @param a dividendo.
-   * @param b divisor.
-   * @return cociente.
-   */
-  public static Fraccion dividir(int a, Fraccion b) { 
-    return dividir(new Fraccion(a, 1), b);
-  }
-  
-  /**
-   * Divide el objeto Fracción actual por la fracción recibida como parámetro y actualiza numerador y denominador.
-   * @param a
-   */
-  public void divide(Fraccion a) {
-    Fraccion f = dividir(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-
-  /**
-   * Divide el objeto Fracción actual por el entero recibido como parámetro y actualiza numerador y denominador.
-   * @param a
-   */
-  public void divide(int a) {
-    Fraccion f = dividir(this, a);
-    this.num = f.num;
-    this.den = f.den;
-  }
-  
-  /**
-   * Calcula el máximo común divisor de dos números.
-   * @param m primer número.
-   * @param n segundo número.
-   * @return máximo común divisor.
-   */
-  private static int mcd(int m, int n) {
-    int u = Math.abs(m);
-    int v = Math.abs(n);
-    if (v == 0) {
-      return u;
-    }
-    int r;
-    while (v != 0) {
-      r = u % v;
-      u = v;
-      v = r;
-    }
-    return u;
-  }
-
-  /**
-   * Simplifica la fracción.
-   */
-  public void simplifica() {
-    int d = mcd(this.num, this.den);
-    this.num /= d;
-    this.den /= d;
-  }
-
-  /**
-   * @return Resultado de dividir numerador entre denominador.
-   */
-  public double resultado() {
-    return (double) this.num / this.den;
-  }
-  
-  public String toString() {
-    return this.num + "/" + this.den;
-  }
-  
-  public boolean equals(Fraccion a) {
-    return (this.num*a.den == this.den*a.num);   
-  }
-  
-  public boolean equals(int a) {
-    return equals(new Fraccion(a, 1));   
   }
 
   @Override
-  public int compareTo(Fraccion a) {
-    return (this.num*a.den - a.num*this.den);
+  public String toString() {
+    return this.numerador + "/" + this.denominador;
   }
   
-  public int compareTo(int a) {
-    return this.compareTo(new Fraccion(a, 1));
+  @Override
+  public int compareTo(Fraccion o) {
+    return this.numerador*o.denominador - o.numerador*this.denominador;
   }
   
+  @Override
+  public Fraccion clone() {
+    return new Fraccion(this.numerador,this.denominador);
+  }
+  
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + denominador;
+    result = prime * result + numerador;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Fraccion other = (Fraccion) obj;
+    if (denominador != other.denominador)
+      return false;
+    if (numerador != other.numerador)
+      return false;
+    return true;
+  }
+
+  //resto de métodos
+  
+  public double resultado() {
+    return (double) this.numerador / this.denominador;
+  }
+  
+  public Fraccion multiplicar(int n) {
+    return new Fraccion(this.numerador * n, this.denominador);
+  }
+  
+  public Fraccion multiplicar(Fraccion f) {
+    return new Fraccion(this.numerador * f.getNumerador(), this.denominador * f.getDenominador());
+  }
+  
+  public Fraccion sumar(Fraccion f) {
+    return new Fraccion(this.numerador*f.getDenominador() + f.getNumerador()*this.denominador,
+                        this.denominador*f.getDenominador());
+  }
+  
+  public Fraccion restar(Fraccion f) {
+    return this.sumar(f.multiplicar(-1));
+  }
+  
+  /**
+   * Me simplifica la fracción diviendo numerador y denominador por su MCD.
+   */
+  public void simplifica() {
+    int mcd = Fraccion.maximoComunDivisor(this.numerador, this.denominador);
+    
+    // Simplifico
+    this.numerador /= mcd;
+    this.denominador /= mcd;
+  }
+  
+  /**
+   * @param a
+   * @param b
+   * @return MCD(a,b)
+   */
+  public static int maximoComunDivisor(int a, int b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    
+    while (b > 0) {
+      int r = a % b;
+      a = b;
+      b = r;
+    }
+    return a;
+  }
+
 }
--- org/iesgrancapitan/PROGR/ejercicios/ej04POO/TestFraccion.java
+++ org/iesgrancapitan/PROGR/ejercicios/ej04POO/TestFraccion.java
@@ -1,5 +1,7 @@
 package org.iesgrancapitan.PROGR.ejercicios.ej04POO;
 
+import java.util.Scanner;
+
 /**
  * Test de la clase Fracción.
  * 
@@ -9,105 +11,55 @@ package org.iesgrancapitan.PROGR.ejercicios.ej04POO;
 public class TestFraccion {
 
   public static void main(String[] args) {
-    Fraccion x = new Fraccion(4, 5);
+    Scanner s = new Scanner(System.in);
+    
+    Fraccion x = new Fraccion(1, 3);
     Fraccion y = new Fraccion(7, 2);
 
+    // Escribimos las dos fracciones
+    System.out.println("Fracción 1: " + x + " con numerador " + x.getNumerador()
+                                          + " y denominador " + x.getDenominador());
+    System.out.println("Fracción 2: " + y + " con numerador " + y.getNumerador() 
+                                          + " y denominador " + y.getDenominador() + "\n");
+    
+    // Cambiamos la primera fracción
+    System.out.print("Nuevo numerador y denominador para " + x + ": ");
+    x.setNumerador(s.nextInt());
+    x.setDenominador(s.nextInt());
+    System.out.println("Nueva fracción: " + x);
+    x.simplifica();
+    System.out.println("Simplificada..: " + x + "\n");
+    
+    // Probamos resultado
     System.out.println(x + " = " + x.resultado());
     System.out.println(y + " = " + y.resultado());
     System.out.println();
-    
+
     System.out.println("Probamos operaciones aritméticas:");
-    System.out.println(x + " + " + y + " = " + Fraccion.sumar(x, y));
-    System.out.println(x + " + 1 = " + Fraccion.sumar(x, 1));
-    System.out.println("1 +" + x + " = " + Fraccion.sumar(1, x));
-    System.out.println();
-    System.out.println(x + " - " + y + " = " + Fraccion.restar(x, y));
-    System.out.println(x + " - 1 = " + Fraccion.restar(x, 1));
-    System.out.println("1 - " + x + " = " + Fraccion.restar(1, x));
+    System.out.println(x + " + " + y + " = " + x.sumar(y));
     System.out.println();
 
-    System.out.println(x + " * " + y + " = " + Fraccion.multiplicar(x, y));
-    System.out.println(x + " / " + y + " = " + Fraccion.dividir(x, y));
+    System.out.println(x + " - " + y + " = " + x.restar(y));
+    System.out.println();
+
+    System.out.println(x + " * " + y + " = " + x.multiplicar(y));
     System.out.println();
 
     System.out.println("Probamos multiplicación y división escalar:");
-    System.out.println(x + " * 2 = " + Fraccion.multiplicar(x, 2));
-    System.out.println("2 * " + x + " = " + Fraccion.multiplicar(2, x));
-    System.out.println(x + " / 2 = " + Fraccion.dividir(x, 2));
-    System.out.println("2 / " + x + " = " + Fraccion.dividir(2, x));
+    System.out.println(x + " * 2 = " + x.multiplicar(2));
     System.out.println();
+    
+    System.out.println("Probamos el máximo común divisor de 45 y 75: " + Fraccion.maximoComunDivisor(45, 75));
 
     System.out.println("Probamos operaciones relacionales:");
-    Fraccion x2 = new Fraccion(x.getNum(), x.getDen());
-    Fraccion x3 = new Fraccion(2*x.getNum(), 2*x.getDen());     //x y x3 son equivalentes
+    Fraccion x2 = new Fraccion(x.getNumerador(), x.getDenominador());
+    Fraccion x3 = new Fraccion(2*x.getNumerador(), 2*x.getDenominador());     //x y x3 son equivalentes
     System.out.println(x + " == " + x2 + " es " + (x == x2));
-    System.out.println(x + ".equals(" + x2 + ") es " + x.equals(x2));
-    System.out.println(x + ".equals(" + x3 + ") es " + x.equals(x3));
-    System.out.println(x + ".equals(" + y +  ") es " + x.equals(y));
     System.out.println(x + ".compareTo(" + y + ") es " + x.compareTo(y));
     System.out.println(y + ".compareTo(" + x + ") es " + y.compareTo(x));
     System.out.println(x + ".compareTo(" + x2 + ") es " + x.compareTo(x2));
     System.out.println(x + ".compareTo(" + x3 + ") es " + x.compareTo(x3));
     System.out.println();
-
-    Fraccion c = new Fraccion(1,1);
-    System.out.println(x + ".equals(1) es " + x.equals(1));
-    System.out.println(c + ".equals(1) es " + c.equals(1));
-    System.out.println(x + ".compareTo(1) es " + x.compareTo(1));
-    System.out.println(c + ".compareTo(1) es " + c.compareTo(1));
-    System.out.println();
-    
-    System.out.println("Modificamos la fracción " + x + ":");
-    System.out.print("- Incrementamos 2/3: ");
-    x.incrementa(new Fraccion(2,3));
-    System.out.println(x);
-    System.out.print("- Incrementamos 1:   ");
-    x.incrementa(1);
-    System.out.println(x);
-    System.out.print("- Decrementamos 1:   ");
-    x.decrementa(1);
-    System.out.println(x);
-    System.out.print("- Decrementamos 2/3: ");
-    x.decrementa(new Fraccion(2,3));
-    System.out.println(x);
-    System.out.println();
-
-    System.out.println("Modificamos la fracción " + y + ":");
-    System.out.print("- Multiplicamosx2/3: ");
-    y.multiplica(new Fraccion(2, 3));
-    System.out.println(y);
-    System.out.print("- Dividimos por 2/3: ");
-    y.divide(new Fraccion(2, 3));
-    System.out.println(y);
-    System.out.print("- Multiplicamos x 3: ");
-    y.multiplica(3);
-    System.out.println(y);
-    System.out.print("- Dividimos entre 3: ");
-    y.divide(3);
-    System.out.println(y);
-    System.out.println();
-    
-    // Denominador 0
-    System.out.println("Intentamos poner cero en el denominador de " + x);
-    try {
-      x.setDen(0);
-    } catch (ArithmeticException e) {
-      System.err.println("ERROR: No se puede poner cero en el denominador");
-    }
-    finally {
-      System.out.println("Resultado: " + x);
-    }
-    System.out.println();
-    
-    System.out.println("Intentamos dividir "+ x + " entre cero");
-    try {
-      x.divide(0);
-    } catch (ArithmeticException e) {
-      System.err.println("ERROR: No se puede dividir por cero");
-    }
-    finally {
-      System.out.println("Resultado: "+x);
-    }
   }
 
 }
