d42ab178a80dbf2a7457cbfb42104992bbcdab7f
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-03-01 10:20:48 +0100

    Cuadrado con excepciones (no marcadas)

---
+++ org/iesgrancapitan/PROGR/ejercicios/ej04POO/CuadradoIncorrectoException.java
@@ -0,0 +1,9 @@
+package org.iesgrancapitan.PROGR.ejercicios.ej04POO;
+
+@SuppressWarnings("serial")
+public class CuadradoIncorrectoException extends RuntimeException {
+
+  public CuadradoIncorrectoException(String message) {
+    super(message);
+  }
+}
