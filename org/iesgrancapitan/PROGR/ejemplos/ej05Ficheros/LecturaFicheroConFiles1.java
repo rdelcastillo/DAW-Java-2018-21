03749bb076d9195a2f462243f576ff361f077906
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-03-06 14:41:36 +0100

    Ejemplos de manejo de ficheros

---
+++ org/iesgrancapitan/PROGR/ejemplos/ej05Ficheros/LecturaFicheroConFiles1.java
@@ -0,0 +1,22 @@
+package org.iesgrancapitan.PROGR.ejemplos.ej05Ficheros;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class LecturaFicheroConFiles1 {
+
+  public static void main(String[] args) {
+
+    try (var file = Files.newBufferedReader(Paths.get("lineas.txt"))) {
+
+      String linea;
+      while((linea=file.readLine()) != null) {
+        System.out.println(linea);
+      }
+      
+    } catch (IOException e) {
+      System.out.println("No se ha podido abrir el fichero.");
+    }
+  }
+}
