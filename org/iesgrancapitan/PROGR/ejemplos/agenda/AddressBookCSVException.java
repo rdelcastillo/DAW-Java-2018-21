a79d57e622ca354b57eb45f2c269f9f33f04615e
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-03-18 11:40:23 +0100

    Agenda de contactos versión 2.0

---
+++ org/iesgrancapitan/PROGR/ejemplos/agenda/AddressBookCSVException.java
@@ -0,0 +1,9 @@
+package org.iesgrancapitan.PROGR.ejemplos.agenda;
+
+@SuppressWarnings("serial")
+public class AddressBookCSVException extends Exception {
+
+  public AddressBookCSVException(String message) {
+    super(message);
+  }
+}
---
+++ org/iesgrancapitan/PROGR/ejemplos/agenda/AddressBookXMLException.java
@@ -0,0 +1,8 @@
+package org.iesgrancapitan.PROGR.ejemplos.agenda;
+
+@SuppressWarnings("serial")
+public class AddressBookXMLException extends Exception {
+  public AddressBookXMLException(String message) {
+    super(message);
+  }
+}
