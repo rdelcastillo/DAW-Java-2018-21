c9f4e20b63350e1e763e142a1a85f01f883b6cbd
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-03-19 10:40:50 +0100

    Desencriptación de un fichero usando el método César

--- org/iesgrancapitan/PROGR/ejercicios/ej05ficheros/Ej02DesencriptaCesar.java
+++ org/iesgrancapitan/PROGR/ejercicios/ej05ficheros/Ej02DesencriptaCesar.java
@@ -1,178 +1,142 @@
 package org.iesgrancapitan.PROGR.ejercicios.ej05ficheros;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * Programa que recibe como parámetro un fichero encriptado con el método César y almacena el resultado en otro, que también
- * pasamos como parámetro, de manera que:
+ * Programa que desencripta un fichero que le pasamos como parámetro y almacena el resultado en otro, 
+ * que también pasamos como parámetro, de manera que:
  * 
- * - Si el programa no recibe dos parámetros termina con un error 1.
- * - Si el programa recibe un solo parámetro guardará la información encriptada en el mismo archivo del que lee, 
- *   pero antes advertirá al usuario de que machacará el archivo origen, dando opción a que la operación no se haga.
- * - Si el fichero origen no existe (da error al abrirlo como lectura) el programa termina con un mensaje de error y código 2.
- * - Si en el fichero destino no se puede escribir da error al abrirlo como lectura) el programa termina con un mensaje de error y
- *   código 2.
+ * - Si el programa no recibe uno o dos parámetros termina con un error 1.
+ * - Si el programa recibe un solo parámetro guardará la información encriptada en el mismo archivo 
+ *   del que lee, pero antes advertirá al usuario de que machacará el archivo origen, dando opción a
+ *   que la operación no se haga.
+ * - Si el fichero origen no existe (da error al abrirlo como lectura) el programa termina con un mensaje
+ *   de error y código 2.
+ * - Si en el fichero destino no se puede escribir (da error al abrirlo como escritura) el programa termina
+ *   con un mensaje de error y código 3.
  *   
- * Para desencriptar necesitas una clave que hay que pedir al usuario.
- *   
- * @author rafa
- *
+ * Para desencriptar usamos el método César, necesitaremos una clave que hay que pedir al usuario.
+ * 
+ * Fíjate que este programa usa gran parte de los métodos del ejercicio que encripta, ¿qué se te ocurre
+ * hacer para que no sea necesario duplicar los métodos?
+ * 
  */
 
 public class Ej02DesencriptaCesar {
-  public static void main(String[] args) throws IOException {
-    // ¿Número de parámetros correcto?
-    if (args.length == 0 || args.length > 2) {
-      System.err.println("Error en el número de parámetros"); // mensaje a la salida de error
-      System.exit(1);
-    }
-            
-    // Averiguamos fichero origen y destino
-    String ficheroOrigen = args[0];
-    String ficheroDestino = ficheroDestino(args);
-        
-    // Abrimos fichero origen
-    BufferedReader bOrigen = bufferOrigen(ficheroOrigen);
-    
-    // Leemos fichero y guardamos en una lista
-    ArrayList<String> lineasOrigen = new ArrayList<String>();
-    String l = bOrigen.readLine();
-    while (l != null) {
-      lineasOrigen.add(l);
-      l = bOrigen.readLine();
-    }
-    bOrigen.close();
 
-    // Pedimos desplazamiento para el método César
-    int desplazamiento = desplazamientoCesar(); 
-        
-    // Abrimos fichero destino (a desencriptar)
-    BufferedWriter bDestino = bufferDestino(ficheroDestino);
+  private static final String LETRAS = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ";
+  private static final int ERROR_ARGS = 1;
+  private static final int ERROR_LECTURA = 2;
+  private static final int ERROR_ESCRITURA = 3;
+
+  public static void main(String[] args) {
+    validarArgumentos(args);
     
-    // Desencriptamos y escribimos
-    for (String linea: lineasOrigen) {
-      bDestino.write(desencriptaCesar(linea, desplazamiento));
-      bDestino.newLine(); // salto de línea, mejor que "\n"
-    }
-    bDestino.close();
+    String origen = args[0];
+    List<String> lineas = getLineasFichero(origen);
     
-    System.out.println("Proceso concluido.");
+    String destino = getFicheroDestino(args);
+    int clave = getClave();
+
+    crearFicheroDesencriptado(destino, lineas, clave);
   }
 
-  /**
-   * 
-   * @param args
-   * @return Nombre del fichero destino
-   */
-  private static String ficheroDestino(String[] args) {
-    Scanner s = new Scanner(System.in);
-    String fichero;
-    
-    if (args.length == 1) {
-      // Fichero origen y destino iguales
-      fichero = args[0];
-
-      // Advertimos de que se machacará el archivo origen
-      System.out.println("Tenga en cuenta que solo ha indicado un nombre de archivo: " + fichero);
-      System.out.println("Está operación machacará los datos de " + fichero);
-
-      // Pedimos confirmación
-      String resp;
-      do {
-        System.out.print("¿Continuamos con la operación? (S/N) ");
-        resp = s.next().toUpperCase();
-      } while (!resp.equals("S") && !resp.equals("N"));
-      if (resp.equals("N")) {
-        System.out.println("Proceso cancelado.");
-        System.exit(0);
-      }
-    } else {
-      fichero = args[1];  // Fichero origen y destino distintos
+  private static void validarArgumentos(String[] args) {
+    if (args.length < 1 || args.length > 2) {
+      finalizarConError("Número de argumentos erróneo.", ERROR_ARGS);
     }
-    return fichero;
-  }
-
-  /**
-   * 
-   * @param ficheroOrigen
-   * @return manejador del fichero abierto para lectura
-   */
-  private static BufferedReader bufferOrigen(String ficheroOrigen) {
-    try {
-      return new BufferedReader(new FileReader(ficheroOrigen));
-    } catch (Exception e) {
-      System.err.println("No se ha podido abrir " + ficheroOrigen);
-      System.exit(2);
-    }
-    return null;
   }
   
-  /**
-   * Pide el desplazamiento para desencriptar usando el método César.
-   * @return desplazamiento.
-   */
-  private static int desplazamientoCesar() {
+  private static List<String> getLineasFichero(String fichero) {
+    List<String> lineas = null;
+    try {
+      lineas = Files.readAllLines(Paths.get(fichero));
+    } catch (IOException e) {
+      finalizarConError("No puedo abrir " + fichero, ERROR_LECTURA);
+    }
+    return lineas;
+  }
+
+  private static String getFicheroDestino(String[] args) {
+    if (args.length == 2) {
+      return args[1];
+    }
+    validarSobreescritura(args[0]);
+    return args[0];
+  }
+
+  private static void validarSobreescritura(String fichero) {
+    System.out.println("Tenga en cuenta que este proceso sobreescribirá " + fichero 
+        + " y perderá la información contenida en el mismo.\n");
+
     Scanner s = new Scanner(System.in);
-    int desplazamiento = 0;
-    boolean seguirEnElCiclo = true;
-    
+    String respuesta;
     do {
-      try {
-        System.out.print("Desplazamiento para la desencriptación usando César: ");
-        desplazamiento = s.nextInt(); 
-        seguirEnElCiclo = false;
-      } catch (Exception e) {
-        System.err.println("Tiene que introducir un valor entero.");
-        s.nextLine();   // quito Intro del buffer de teclado
-      }
-    } while (seguirEnElCiclo);
-    return desplazamiento;
-  }
-  
-  /**
-   * 
-   * @param ficheroDestino
-   * @return manejador del fichero abierto para escritura
-   */
-  private static BufferedWriter bufferDestino(String ficheroDestino) {
-    try {
-      return new BufferedWriter(new FileWriter(ficheroDestino));
-    } catch (Exception e) {
-      System.err.println("No se ha podido abrir para escritura " + ficheroDestino);
-      System.exit(3);
+      System.out.print("¿Está seguro/a? (S/N) ");
+      respuesta = s.nextLine().toUpperCase();
+    } while (!respuesta.equals("S") && !respuesta.equals("N"));
+
+    if (respuesta.equals("N")) {
+      System.out.println("Proceso cancelado.");
+      System.exit(0);
     }
-    return null;
   }
-  
-  /**
-   * Encripta la cadena que le pasamos como parámetro. 
-   * @param cadena
-   * @param desplazamiento
-   * @return cadena encriptada
-   */
-  private static String desencriptaCesar(String cadena, int desplazamiento) {
-    String letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúüñÁÉÍÓÚÜÑ";
+
+  private static int getClave() {
+    Scanner s = new Scanner(System.in);
+    int clave;
+
+    do {
+      System.out.print("Desplazamiento (positivo) para la encriptación usando César: ");
+      clave = s.nextInt();
+    } while (clave <= 0); 
+    
+    return clave;
+  }
+
+  private static void crearFicheroDesencriptado(String destino, List<String> lineas, int clave) {
+    try (var ficheroDesencriptado = new PrintWriter(destino)) {
+      for (String linea: lineas) {
+        String cadenaDesencriptada = getCadenaDesencriptada(linea, clave);
+        ficheroDesencriptado.println(cadenaDesencriptada);
+      }
+      System.out.println("Creado fichero desencriptado: " + destino);
+
+    } catch (IOException e) {
+      finalizarConError("Error al escribir en " + destino, ERROR_ESCRITURA);
+    }
+  }
+
+  private static String getCadenaDesencriptada(String cadena, int desplazamiento) {
     String cadenaDesencriptada = "";
-    for (char caracter: cadena.toCharArray()) {
-      char caracterDesencriptado = caracter;
-      // si el carácter es alfabético, desencriptamos
-      if (letras.contains(Character.toString(caracter))) {
-        int posicionDondeEsta = letras.indexOf(caracter);
-        int posicionCaracterDesencriptado = ((posicionDondeEsta - desplazamiento) % letras.length());
-        if (posicionCaracterDesencriptado < 0) {
-          posicionCaracterDesencriptado = letras.length() + posicionCaracterDesencriptado;
-        }
-        caracterDesencriptado = letras.charAt(posicionCaracterDesencriptado);
-      } 
-      cadenaDesencriptada += caracterDesencriptado;
+    for (char ch: cadena.toCharArray()) {
+      cadenaDesencriptada += getCaracterDesencriptado(ch, desplazamiento);
     }
     return cadenaDesencriptada;
+  }
+
+  private static char getCaracterDesencriptado(char ch, int desplazamiento) {
+    return (Character.isLetter(ch)) ? getCaracterDesplazado(ch, desplazamiento) : ch;
+  }
+  
+  private static char getCaracterDesplazado(char ch, int desplazamiento) {
+    String letras = (Character.isUpperCase(ch)) ? LETRAS : LETRAS.toLowerCase();
+    int posicionCaracter = letras.indexOf(ch);
+    int posicionCaracterDesplazado = (posicionCaracter - desplazamiento) % letras.length();
+    if (posicionCaracterDesplazado < 0) {
+      posicionCaracterDesplazado = letras.length() - posicionCaracterDesplazado;
+    }
+    return letras.charAt(posicionCaracterDesplazado);
+  }
+
+  private static void finalizarConError(String mensajeError, int codigoError) {
+    System.err.println(mensajeError);
+    System.exit(codigoError);
   }
 
 }
