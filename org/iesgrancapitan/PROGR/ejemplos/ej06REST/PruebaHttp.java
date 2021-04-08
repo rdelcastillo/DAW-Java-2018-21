11aecd1ad06680ac3145a915577b19ec773fc898
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-04-08 16:53:00 +0200

    Ejemplo de acceso HTTP

---
+++ org/iesgrancapitan/PROGR/ejemplos/ej06REST/PruebaHttp.java
@@ -0,0 +1,28 @@
+package org.iesgrancapitan.PROGR.ejemplos.ej06REST;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+/**
+ * Ejemplo de acceso HTTP desde Java.
+ * 
+ * @author Rafael del Castillo Gomariz
+ *
+ */
+
+public class PruebaHttp {
+  
+  public static void main(String[] args) throws IOException, InterruptedException {
+    
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder(URI.create("http://example.com")).build();
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    
+    System.out.println("Status Code: " + response.statusCode());
+    System.out.println("HTML devuelto:\n" + response.body());
+  }
+}
