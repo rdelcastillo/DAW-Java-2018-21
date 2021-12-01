74e5c885a38f180a3f585d55c9d4904ae5d8250c
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-12-01 11:03:38 +0100

    Ejemplos de iteradores y expresiones lambda

---
+++ org/iesgrancapitan/PROGR/ejemplos/iteradoresylambda/TestTablaMultiplicarIterable.java
@@ -0,0 +1,24 @@
+package org.iesgrancapitan.PROGR.ejemplos.iteradoresylambda;
+
+public class TestTablaMultiplicarIterable {
+
+  public static void main(String[] args) {
+    int numeroTabla;
+    int filaTabla;
+    
+    numeroTabla = 5;
+    filaTabla = 1;
+    System.out.println("Tabla de multiplicar del " + numeroTabla + ": ");
+    var tabla5Iterable = new TablaMultiplicarIterable(numeroTabla);
+    for (int producto: tabla5Iterable) {
+      System.out.println(filaTabla++ + " x " + numeroTabla + " = " + producto);
+    }
+    
+    numeroTabla = 8;
+    filaTabla = 1;
+    System.out.print("\nTabla de multiplicar del " + numeroTabla + " (15 resultados): ");
+    var tabla8Iterable = new TablaMultiplicarIterable(numeroTabla, 15);
+    tabla8Iterable.forEach(producto -> System.out.print(producto + " "));
+  }
+
+}
