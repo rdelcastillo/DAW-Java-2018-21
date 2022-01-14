5fcbb50827c7415ed6aa6907d5193846929015f7
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2022-01-14 12:16:35 +0100

    Ejercicios de manejo de funciones

--- org/iesgrancapitan/PROGR/ejercicios/ej03funciones/PruebaFuncionesEjercicios1a14.java
+++ org/iesgrancapitan/PROGR/ejercicios/ej03funciones/PruebaFuncionesEjercicios1a14.java
@@ -3,7 +3,10 @@ package org.iesgrancapitan.PROGR.ejercicios.ej03funciones;
 import org.iesgrancapitan.PROGR.ejercicios.ej03funciones.matematicas.FuncionesEj1a14;
 
 /**
- * Probamos clase de funciones de los ejercicios 1 a 14.
+ * Probamos la clase de funciones de los ejercicios 1 a 14.
+ * 
+ * Para hacer las pruebas se usan funciones con varargs a las que les pasamos los parámetros 
+ * de las funciones a evaluar.
  * 
  * @author rafa
  *
@@ -11,34 +14,174 @@ import org.iesgrancapitan.PROGR.ejercicios.ej03funciones.matematicas.FuncionesEj
 
 public class PruebaFuncionesEjercicios1a14 {
   public static void main(String[] args) {
-    System.out.println("Prueba función esPrimo:");
-    System.out.println("-----------------------");
-    System.out.println("2: " + FuncionesEj1a14.esPrimo(2));
-    System.out.println("4: " + FuncionesEj1a14.esPrimo(4));
-    System.out.println("9: " + FuncionesEj1a14.esPrimo(9));
-    System.out.println("17: " + FuncionesEj1a14.esPrimo(17));
-    System.out.println("-4: " + FuncionesEj1a14.esPrimo(-4));
-    System.out.println("-17: " + FuncionesEj1a14.esPrimo(-17));
-    System.out.println();
-    
-    System.out.println("Prueba función siguientePrimo:");
-    System.out.println("------------------------------");
-    System.out.println("2: " + FuncionesEj1a14.siguientePrimo(2));
-    System.out.println("4: " + FuncionesEj1a14.siguientePrimo(4));
-    System.out.println("9: " + FuncionesEj1a14.siguientePrimo(9));
-    System.out.println("17: " + FuncionesEj1a14.siguientePrimo(17));
-    System.out.println("-4: " + FuncionesEj1a14.siguientePrimo(-4));
-    System.out.println("-17: " + FuncionesEj1a14.siguientePrimo(-17));
-    System.out.println();
-    
-    System.out.println("Prueba función volteado:");
-    System.out.println("------------------------");
-    System.out.println("2: " + FuncionesEj1a14.voltea(2));
-    System.out.println("23: " + FuncionesEj1a14.voltea(23));
-    System.out.println("327: " + FuncionesEj1a14.voltea(327));
-    System.out.println("2098: " + FuncionesEj1a14.voltea(2098));
-    System.out.println("-234: " + FuncionesEj1a14.voltea(-234));
-    System.out.println();
+    pruebaEsPrimo(2, 4, 9, 17, -4, -17, 787);
+    pruebaSiguientePrimo(2, 4, 9, 17, -4, -17);
+    pruebaPotencia(new double[] {2,8}, new double[] {2,-1}, new double[] {158,0}, new double[] {0.5,-2});
+    pruebaDigitos(200, 49, -9987, -17, 56, 11117);
+    pruebaQuitaPorDetras(new int[] {158,1}, new int[] {-25687,2}, new int[] {389887,4});
+    pruebaQuitaPorDelante(new int[] {158,1}, new int[] {-25687,2}, new int[] {389887,4});
+    pruebaVoltea(2, 23, 327, 2098, -234);
+    pruebaTrozoDeNumero(new int[] {158,0,2}, new int[] {5878,1,3}, new int[] {9958,2,4});
+    pruebaPegaPorDetras(new int[] {158,0}, new int[] {18,4}, new int[] {0,9});
+    pruebaPegaPorDelante(new int[] {158,0}, new int[] {18,4}, new int[] {0,9});
+    pruebaDigitoN(new int[] {158,0}, new int[] {798,1}, new int[] {986,2});
+    pruebaPosicionDeDigito(new int[] {158,8}, new int[] {798,9}, new int[] {986,3});
+    pruebaJuntaNumeros(new int[] {158,10}, new int[] {798,199}, new int[] {86,1873});
+    pruebaEsCapicua(25, 313, 2222, 3223, -3883, 578);
+  }
+
+  private static void pruebaEsPrimo(int... parametros) {
+    String nombreFuncion = "esPrimo";
+    printFuncion(nombreFuncion);
+    for (int n: parametros) {
+      System.out.println(funcion(nombreFuncion, n) + " --> " + FuncionesEj1a14.esPrimo(n));
+    }
+  }
+
+  private static void pruebaSiguientePrimo(int... parametros) {
+    String nombreFuncion = "siguientePrimo";
+    printFuncion(nombreFuncion);
+    for (int n: parametros) {
+      System.out.println(funcion(nombreFuncion, n) + " --> " + FuncionesEj1a14.siguientePrimo(n));
+    }
+  }
+  
+  private static void pruebaPotencia(double[]... conjuntoParametros) {
+    String nombreFuncion = "potencia";
+    printFuncion(nombreFuncion);
+    for (double[] parametros : conjuntoParametros) {
+      double base = parametros[0];
+      int exponente = (int) parametros[1]; 
+      System.out.println(nombreFuncion + "(" + base + ", " + exponente + ") --> " 
+          + FuncionesEj1a14.potencia(base, exponente));
+    }     
+  }
+  
+  private static void pruebaDigitos(int... parametros) {
+    String nombreFuncion = "digitos";
+    printFuncion(nombreFuncion);
+    for (int n: parametros) {
+      System.out.println(funcion(nombreFuncion, n) + " --> " + FuncionesEj1a14.digitos(n));
+    }    
+  }
+
+  private static void pruebaQuitaPorDetras(int[]... conjuntoParametros) {
+    String nombreFuncion = "quitaPorDetras";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int digitosAQuitar = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero, digitosAQuitar) + " --> " 
+          + FuncionesEj1a14.quitaPorDetras(numero, digitosAQuitar));
+    }     
+  }
+  
+  private static void pruebaQuitaPorDelante(int[]... conjuntoParametros) {
+    String nombreFuncion = "quitaPorDelante";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int digitosAQuitar = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero, digitosAQuitar) + " --> " 
+          + FuncionesEj1a14.quitaPorDelante(numero, digitosAQuitar));
+    }     
+  }
+  
+  private static void pruebaVoltea(int... parametros) {
+    String nombreFuncion = "voltea";
+    printFuncion(nombreFuncion);
+    for (int n: parametros) {
+      System.out.println(funcion(nombreFuncion, n) + " --> " + FuncionesEj1a14.voltea(n));
+    }    
+  }
+
+  private static void pruebaTrozoDeNumero(int[]... conjuntoParametros) {
+    String nombreFuncion = "trozoDeNumero";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int inicio = parametros[1];
+      int fin = parametros[2];
+      System.out.println(funcion(nombreFuncion, numero, inicio, fin) + " --> " + 
+          FuncionesEj1a14.trozoDeNumero(numero, inicio, fin));
+    }    
+  }
+
+  private static void pruebaPegaPorDetras(int[]... conjuntoParametros) {
+    String nombreFuncion = "pegaPorDetras";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int digitoAPegar = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero, digitoAPegar) + " --> " 
+          + FuncionesEj1a14.pegaPorDetras(numero, digitoAPegar));
+    }
+  }
+
+  private static void pruebaPegaPorDelante(int[]... conjuntoParametros) {
+    String nombreFuncion = "pegaPorDelante";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int digitoAPegar = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero, digitoAPegar) + " --> " 
+          + FuncionesEj1a14.pegaPorDelante(numero, digitoAPegar));
+    }
+  }
+
+  private static void pruebaDigitoN(int[]... conjuntoParametros) {
+    String nombreFuncion = "digitoN";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int posicion = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero, posicion) + " --> " 
+          + FuncionesEj1a14.digitoN(numero, posicion));
+    }
+  }
+
+  private static void pruebaPosicionDeDigito(int[]... conjuntoParametros) {
+    String nombreFuncion = "posicionDeDigito";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero = parametros[0];
+      int digito = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero, digito) + " --> " 
+          + FuncionesEj1a14.posicionDeDigito(numero, digito));
+    }
+  }
+
+  private static void pruebaJuntaNumeros(int[]... conjuntoParametros) {
+    String nombreFuncion = "juntaNumeros";
+    printFuncion(nombreFuncion);
+    for (int[] parametros : conjuntoParametros) {
+      int numero1 = parametros[0];
+      int numero2 = parametros[1];
+      System.out.println(funcion(nombreFuncion, numero1, numero2) + " --> " 
+          + FuncionesEj1a14.juntaNumeros(numero1, numero2));
+    }
+  }
+
+  private static void pruebaEsCapicua(int... parametros) {
+    String nombreFuncion = "esCapicua";
+    printFuncion(nombreFuncion);
+    for (int n: parametros) {
+      System.out.println(funcion(nombreFuncion, n) + " --> " + FuncionesEj1a14.esCapicua(n));
+    }  
+  }
+  
+  public static void printFuncion(String funcion) {
+    System.out.println("\nPrueba función " + funcion + "()");
+    System.out.println("-".repeat(funcion.length() + 17));
+  }
+
+  private static String funcion(String nombreFuncion, int ... parametros) {
+    String nombre = nombreFuncion + "(" + parametros[0];
+    for (int i = 1; i < parametros.length; i++) {
+      nombre += ", " + parametros[i];
+    }
+    nombre += ")";
+    return nombre;
   }
 
 }
--- org/iesgrancapitan/PROGR/ejercicios/ej03funciones/matematicas/FuncionesEj1a14.java
+++ org/iesgrancapitan/PROGR/ejercicios/ej03funciones/matematicas/FuncionesEj1a14.java
@@ -1,113 +1,227 @@
 package org.iesgrancapitan.PROGR.ejercicios.ej03funciones.matematicas;
 
 /**
- * Biblioteca de funciones matemáticas que contiene:
+ * <p>Biblioteca de funciones matemáticas.</p>
+ * <ol>
+ * <li>esCapicua: Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en
+ * caso contrario.</li>
  * 
- * 1. esCapicua: Devuelve verdadero si el número que se pasa como parámetro es capicúa 
- * y falso en caso contrario.
+ * <li>esPrimo: Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso
+ * contrario.</li>
  * 
- * 2. esPrimo: Devuelve verdadero si el número que se pasa como parámetro es primo y 
- * falso en caso contrario.
+ * <li>siguientePrimo: Devuelve el menor primo que es mayor al número que se pasa como parámetro.</li>
  * 
- * 3. siguientePrimo: Devuelve el menor primo que es mayor al número que se pasa como 
- * parámetro.
+ * <li>potencia: Dada una base y un exponente devuelve la potencia.</li>
  * 
- * 4. potencia: Dada una base y un exponente devuelve la potencia.
+ * <li>digitos: Cuenta el número de dígitos de un número entero.</li>
  * 
- * 5. digitos: Cuenta el número de dígitos de un número entero.
+ * <li>voltea: Le da la vuelta a un número.</li>
  * 
- * 6. voltea: Le da la vuelta a un número.
+ * <li>digitoN: Devuelve el dígito que está en la posición n de un número entero. Se empieza contando
+ * por el 0 y de izquierda a derecha.</li>
  * 
- * 7. digitoN: Devuelve el dígito que está en la posición n de un número entero. 
- * Se empieza contando por el 0 y de izquierda a derecha.
+ * <li>posicionDeDigito: Da la posición de la primera ocurrencia de un dígito dentro de un número
+ * entero. Si no se encuentra, devuelve -1.</li>
  * 
- * 8. posicionDeDigito: Da la posición de la primera ocurrencia de un dígito dentro 
- * de un número entero. Si no se encuentra, devuelve -1.
+ * <li>quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha).</li>
  * 
- * 9. quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha).
+ * <li>quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda).</li>
  * 
- * 10. quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda).
+ * <li>pegaPorDetras: Añade un dígito a un número por detrás.</li>
  * 
- * 11. pegaPorDetras: Añade un dígito a un número por detrás.
+ * <li>pegaPorDelante: Añade un dígito a un número por delante.</li>
  * 
- * 12. pegaPorDelante: Añade un dígito a un número por delante.
+ * <li>trozoDeNumero: Toma como parámetros las posiciones inicial y final dentro de un número y
+ * devuelve el trozo correspondiente.</li>
  * 
- * 13. trozoDeNumero: Toma como parámetros las posiciones inicial y final dentro de un 
- * número y devuelve el trozo correspondiente.
- * 
- * 14. juntaNumeros: Pega dos números para formar uno.
- * 
- * Ejercicios del libro "Aprende Java con Ejercicios. Edición 2019".
+ * <li>juntaNumeros: Pega dos números para formar uno.</li>
+ * </ol>
+ * <p>Ejercicios del libro "Aprende Java con Ejercicios. Edición 2019".</p>
  * 
  * @author Clase 1ºDAW IES Gran Capitán, curso 2019/20.
  *
  */
 public class FuncionesEj1a14 {
-  
+
   /**
-   * Devuelve verdadero si el número que se pasa como parámetro es primo y falso en
-   * caso contrario.
-   * @param n número a comprobar
+   * Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso contrario.
+   * 
+   * @param numero número a comprobar
    * @return verdadero o falso
    */
-  public static boolean esPrimo(int n) {
-    boolean creoQueEsPrimo = true;
-    
-    // casos triviales 
-    if (n==2) { // 2 es primo
+  public static boolean esPrimo(int numero) {
+    // casos triviales
+    if (numero == 2) { // 2 es primo
       return true;
-    } else if (n<=1 || n%2==0) { // par no es primo
+    } else if (numero <= 1 || numero % 2 == 0) { // par no es primo
       return false;
     }
-    
-    // proceso
-    for (int i=3; i<=Math.sqrt(n) && creoQueEsPrimo; i+=2) {
-      if (n%i == 0) {
-        creoQueEsPrimo = false;
+    // proceso normal
+    for (int i = 3; i <= Math.sqrt(numero); i += 2) {
+      if (numero % i == 0) {
+        return false;
       }
     }
-
-    // fin
-    return creoQueEsPrimo;    
+    return true; // si llego aquí el número es primo
   }
-  
+
   /**
    * Devuelve el menor primo que es mayor al número que se pasa como parámetro.
    * 
-   * @param n número a comprobar
+   * @param numero número a comprobar
    * @return primo
    */
-  public static int siguientePrimo(int n) {
-    int primoSiguiente = n+1;   // candidato a primo siguiente
-    
-    // proceso para encontrar elprimo siguiente
-    while (!esPrimo(primoSiguiente)) {
+  public static int siguientePrimo(int numero) {
+    int primoSiguiente = numero + 1; // candidato a primo siguiente
+    while (!esPrimo(primoSiguiente)) { // proceso para encontrar el primo siguiente
       primoSiguiente++;
     }
-    
-    // fin
     return primoSiguiente;
+  }
+  
+  /**
+   * Dada una base y un exponente devuelve la potencia.
+   * @param base
+   * @param exponente
+   * @return base ** exponente
+   */
+  public static double potencia(double base, int exponente) {
+    double resultado = 1;
+    for (int n = 1; n <= Math.abs(exponente); n++) {
+      resultado *= base;
+    }
+    return (exponente >= 0) ? resultado: 1/resultado;
   }
 
   /**
+   * Cuenta el número de dígitos de un número entero.
+   * 
+   * @param n
+   * @return número de digitos de num
+   */
+  public static int digitos(int numero) {
+    int digitos = 0;
+    int n = numero;    // podríamos haber usado numero, pero no es conveniente
+    do {
+      digitos++;
+      n = n / 10;
+    } while (n != 0);
+    return digitos;
+  }
+  
+  /**
+   * Le quita a un número n dígitos por detrás (por la derecha).
+   * @param numero número al que hay que quitar los dígitos.
+   * @param digitosAQuitar número de dígitos a quitar por detrás.
+   * @return número después de haberle quitado por detrás los dígitos.
+   */
+  public static int quitaPorDetras(int numero, int digitosAQuitar) {
+    return numero / (int) potencia(10, digitosAQuitar);
+  }
+  
+  /**
+   * Le quita a un número n dígitos por delante (por la izquierda).
+   * @param numero número al que hay que quitar los dígitos.
+   * @param digitosAQuitar número de dígitos a quitar por delante.
+   * @return número después de haberle quitado por delante los dígitos.
+   */
+  public static int quitaPorDelante(int numero, int digitosAQuitar) {
+    return numero % (int) potencia(10, digitos(numero)-digitosAQuitar);
+  }
+  
+  /**
+   * Toma como parámetros las posiciones inicial y final dentro de un número y devuelve el trozo correspondiente.
+   * @param numero
+   * @param inicio
+   * @param fin
+   * @return número troceado
+   */
+  public static int trozoDeNumero(int numero, int inicio, int fin) {
+    int numeroSinTrozoFinal = quitaPorDetras(numero, digitos(numero)-fin);
+    int numeroTroceado = quitaPorDelante(numeroSinTrozoFinal, inicio);
+    return numeroTroceado;
+  }
+  
+  /**
+   * Devuelve el dígito que está en la posición n de un número entero. Se empieza contando por el 0 y de izquierda a derecha.
+   * @param numero
+   * @param posicion
+   * @return
+   */
+  public static int digitoN(int numero, int posicion) {
+    return trozoDeNumero(numero, posicion, posicion+1);
+  }
+  
+  /**
+   * Da la posición de la primera ocurrencia de un dígito dentro de un número entero. Si no se encuentra, devuelve -1.
+   * @param numero
+   * @param digito
+   * @return posición del dígito, -1 si no se encuentra.
+   */
+  public static int posicionDeDigito(int numero, int digito) {
+    for (int posicion = 0; posicion < digitos(numero); posicion++) {
+      if (digitoN(numero, posicion) == digito) {
+        return posicion;
+      }
+    }
+    return -1;
+  }
+  
+  /**
+   * Añade un dígito a un número por detrás.
+   * @param numero
+   * @param digito 
+   * @return número resultado.
+   */
+  public static int pegaPorDetras(int numero, int digito) {
+    return numero * 10 + digito;
+  }
+  
+  /**
+   * Añade un dígito a un número por delante.
+   * @param numero
+   * @param digito 
+   * @return número resultado.
+   */
+  public static int pegaPorDelante(int numero, int digito) {
+    return digito * (int) potencia(10, digitos(numero)) + numero;
+  }
+  
+  /**
+   * Pega dos números para formar uno.
+   * @param numero1
+   * @param numero2
+   * @return número resultado de unir los dos números.
+   */
+  public static int juntaNumeros(int numero1, int numero2) {
+    int numeroResultado = numero1;
+    for (int posicion = 0; posicion < digitos(numero2); posicion++) {
+      numeroResultado = pegaPorDetras(numeroResultado, digitoN(numero2, posicion));
+    }
+    return numeroResultado;
+  }
+  
+  /**
    * Le da la vuelta a un número.
-   * @param n número a voltear
+   * 
+   * @param numero número a voltear
    * @return número volteado
    */
-  public static int voltea(int n) {
-    String numero = Integer.toString(Math.abs(n));
-    String volteado = "";
-
-    // volteo la cadena numérica sin signo
-    for (int i=numero.length()-1; i>=0; i--) {
-      volteado += numero.substring(i,i+1);
+  public static int voltea(int numero) {
+    int numeroVolteado = digitoN(numero, digitos(numero)-1);
+    for (int posicion = digitos(numero)-2; posicion >= 0; posicion--) {
+      numeroVolteado = pegaPorDetras(numeroVolteado, digitoN(numero, posicion));
     }
-    
-    // ahora tengo cuenta el signo para la devolución
-    if (n>=0) {
-      return Integer.parseInt(volteado);
-    } else {
-      return -1 * Integer.parseInt(volteado);
-    }
+    return numeroVolteado;
   }
+  
+  /**
+   * Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
+   * @param numero
+   * @return true si es capicúa, falso en caso contrario.
+   */
+  public static boolean esCapicua(int numero) {
+    return numero == voltea(numero);
+  }
+ 
 }
